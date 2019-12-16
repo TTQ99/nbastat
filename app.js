07781e6998788a00e0936dcda25010e70f44a897
tantingqiang <tantingqiang@viomi.com.cn>
2019-12-16 17:51:57 +0800

    refactor: 1111111111

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const catchError = require('./middlewares/exception')
 const { Spider } = require('./app/spider/index')
 const { fSpider } = require('./app/spider/football')
 const cors = require('@koa/cors')
+const static_ = require('koa-static')
+
 
 
 Spider.init()
@@ -13,11 +15,13 @@ Spider.init()
 
 const app = new Koa()
 app.use(cors());
-
+app.use(static_(
+  './'
+))
 app.use(catchError)
 app.use(parser())
 InitManager.initCore(app)
 
-app.listen('3000', function () {
+app.listen('8080', function () {
   console.log('server listen prot:3000');
 })
